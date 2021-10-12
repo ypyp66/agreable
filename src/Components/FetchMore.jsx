8b2a8a32ac1b86e709b9e43ebda6400bdfb8b312
kitebeen <ypyp66@naver.com>
2021-10-13 02:15:09 +0900

    무한 스크롤 구현 #4

---
+++ src/Components/FetchMore.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useMemo, useRef } from "react";
+
+function FetchMore({ setPage }) {
+  const observableTrigger = useRef(null);
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([{ isIntersecting }]) => {
+        isIntersecting && setPage((prev) => prev + 1);
+      }),
+    [setPage]
+  );
+
+  useEffect(() => {
+    observer.observe(observableTrigger.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [observer]);
+
+  return <div ref={observableTrigger} style={{ marginTop: "-5px" }} />;
+}
+
+export default FetchMore;
--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,44 +1,102 @@
-import React from "react";
-import { useQuery } from "react-query";
+import useLocalStorage from "Hooks/useLocalStorage";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getLists } from "Utils/Api";
 import FetchMore from "./FetchMore";
 import ListItem from "./ListItem";
 
 function Lists() {
-  const { isLoading, error, data } = useQuery("lists", () => getLists(1));
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
 
-  console.log({ data });
+  const [error, setError] = useState(false);
+  const [isLast, setIsLast] = useState(false);
+  const [cartStates, setCartStates] = useLocalStorage("cart");
+
+  const handleAdd = (data) => {
+    const check = cartStates.filter((item) => item.id === data.id).length > 0;
+
+    if (check) {
+      alert("이미 장바구니에 있습니다. 수량을 증가합니다.");
+      setCartStates(
+        cartStates.map((item) =>
+          item.id === data.id ? { ...item, amount: item.amount + 1 } : item
+        )
+      );
+      return;
+    }
+
+    data.amount = 1;
+    data.isChecked = true;
+
+    setCartStates(cartStates.concat(data));
+  };
+
+  useEffect(() => {
+    if (isLast) return;
+
+    (async function () {
+      try {
+        const result = await getLists(page);
+
+        if (result.length === 0) {
+          setIsLast(true);
+        }
+
+        setData((prev) => [...prev, ...result]);
+      } catch (e) {
+        setError(e);
+      }
+    })();
+  }, [page, isLast]);
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
-    <Container>
-      {isLoading ? (
-        <div>로딩중</div>
+    <>
+      <Container>
+        {data.length > 0 && (
+          <Inner>
+            {data.map((list) => (
+              <ListItem key={list.id} data={list} handleAdd={handleAdd}>
+                {list.itemName}
+              </ListItem>
+            ))}
+          </Inner>
+        )}
+      </Container>
+      {isLast ? (
+        <div>마지막 컨텐츠입니다.</div>
       ) : (
-        <Inner>
-          {data.map((list) => (
-            <ListItem key={list.id} data={list}>
-              {list.itemName}
-            </ListItem>
-          ))}
-        </Inner>
+        <FetchMore setPage={setPage} />
       )}
-      {error && <div>error발생</div>}
-      <FetchMore />
-    </Container>
+    </>
   );
 }
 
-export default Lists;
+export default React.memo(Lists);
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
+
+  @media ${(props) => props.theme.tablet} {
+    width: 100%;
+    align-items: center;
+  }
 `;
 
 const Inner = styled.div`
   width: 1200px;
   display: grid;
   grid-template-columns: repeat(4, 250px);
-  column-gap: 20px;
   justify-content: space-between;
+
+  @media ${(props) => props.theme.tablet} {
+    width: 100%;
+    grid-template-columns: repeat(2, 45%);
+    justify-items: center;
+  }
 `;
